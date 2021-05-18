53b22e3d4070c0fa3871cc5fe32d0adede7ce9ae
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:01:09 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"ее дети",
+        options:["his children", "her children", "its children"],
+        answer: 1
+    },
+    {
+     q:"их комната",
+        options:["their room", "our room", "your room"],
+        answer: 0   
+    },
+    {
+        q:"наша планета",
+        options:["their planet", "her planet", "our planet"],
+        answer: 2
+    },
+    {
+        q:"мое платье",
+        options:[ "her dress", "its dress", "my dress"],
+        answer: 2
+    },
+    {
+        q:"его машина",
+        options:["his car", "its car", "her car",],
+        answer: 0
+    },
+    {
+        q:"ваши очки",
+        options:["our glasses", "your glasses", "their glasses"],
+        answer: 1
+    },
+    {
+        q:"твой телефон",
+        options:["your phone", "our phone", "their phone"],
+        answer: 0
+    },
+    {
+        q:"наши родственники",
+        options:["your relatives", "our relatives", "my relatives"],
+        answer: 1
+    },
+    {
+        q:"их друзья",
+        options:["his friends", "their friends", "your friends"],
+        answer: 1
+    },
+    {
+        q:"его идея",
+        options:["its idea", "my idea", "his idea"],
+        answer: 2
+    }
+
+]
